836686abfbf7b749f8d2016f2cf465948e3289b5
linnabraham <linn.official@gmail.com>
2024-06-20 12:48:14 +0530

    Add script for creating mpd queue from recently modified files

---
+++ experimental/arch-thinkpad/mpc-add-recent.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+# Script to add music files modified in the last 30 days to mpd play queue
+
+mpc clear
+cd ~/moosic &&
+
+# add music modified in the last 30 days
+find . -type f -mtime -30 -print0 | while IFS= read -r -d '' file; do
+    mpc add "$(echo "$file" | sed 's|^\./||')"
+done
+
