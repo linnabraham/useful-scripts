d6d57026cd5d01919be63845c6ae48b29f0bf9c5
linnabraham <linn.official@gmail.com>
2024-07-17 11:34:59 +0530

    Add script for switching to new workspace in i3

---
+++ experimental/switch-i3-workspace.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+# Switch to a new workspace automatically without first specifying the number
+# Avoids certain workspaces which are reserved
+
+i3-msg workspace $(
+    next_ws=$((
+        $(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | grep -v '^10$' | sort -rn | head -1) + 1
+    ))
+    if [ "$next_ws" -eq 10 ]; then
+        next_ws=11
+    fi
+    echo $next_ws
+)
