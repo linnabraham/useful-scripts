65334b93377c9742bf10f69d6f6c55bbe84e93aa
linnabraham <linn.official@gmail.com>
2024-07-18 14:06:50 +0530

    Change path to store screenshots

--- openbox-screenshot.sh
+++ openbox-screenshot.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
 timestamp=$(date '+%Y%m%d-%H%M%S')
-import -silent -window root ~/Pictures/"$timestamp".png
-xclip -selection clipboard -t image/png -i ~/Pictures/"$timestamp".png
+import -silent -window root ~/Pictures/screenshots/"$timestamp".png
+xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/"$timestamp".png
 notify-send "Copied file $timestamp.png to clipboard"
