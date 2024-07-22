22faa58ae792e74a35d785cac8cc3653f7401479
linnabraham <linn.official@gmail.com>
2024-07-23 04:42:14 +0530

    Fix: missing space in if block

--- experimental/switch-i3-workspace.sh
+++ experimental/switch-i3-workspace.sh
@@ -6,7 +6,7 @@ i3-msg workspace $(
     next_ws=$((
         $(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | grep -v '^10$' | grep -v '^9$' | sort -rn | head -1) + 1
     ))
-    if [ "$next_ws" -eq 10 ] || [ "$next_ws" -eq 9] ; then
+    if [ "$next_ws" -eq 10 ] || [ "$next_ws" -eq 9 ] ; then
         next_ws=11
     fi
     echo $next_ws
