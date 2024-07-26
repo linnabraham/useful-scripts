2e1da4880123dfb694bbab570ac03864bc4f9319
linnabraham <linn.official@gmail.com>
2024-07-26 08:58:13 +0530

    Add script for displaying audio playing in polybar

---
+++ experimental/poly-playerctl.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# If no audio instance is running leave empty
+# If player is in paused state display a play button
+# Else display the title of the audio
+
+playerctlstatus=$(playerctl status 2> /dev/null)
+
+if [[ $playerctlstatus ==  "" ]]; then
+    echo ""
+elif [[ $playerctlstatus =~ "Playing" ]]; then
+    echo "%{A1:playerctl pause:}%{A6:playerctl next:}%{A8:playerctl previous:} `playerctl metadata title`%{A}%{A}%{A}"
+else
+    echo "%{A1:playerctl play:}%{A}"
+fi
