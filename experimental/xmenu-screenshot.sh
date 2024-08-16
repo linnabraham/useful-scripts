054875ee1afd589b8b6d0cf3ff569554a7ca91b3
linnabraham <linn.official@gmail.com>
2024-08-16 09:32:13 +0530

    Make notification disappear quickly

--- experimental/xmenu-screenshot.sh
+++ experimental/xmenu-screenshot.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-notify-send -t 0 "Notification" "Wait 5 seconds for a screenshot.." && sleep 5
+notify-send -t 2000 "Notification" "Wait 5 seconds for a screenshot.." && sleep 5
 timestamp=$(date '+%Y%m%d-%H%M%S')
 import -silent -window root ~/Pictures/screenshots/"$timestamp".png
-xclip -selection clipboard -t image/png -i ~/Pictures/"$timestamp".png
-notify-send "Copied file $timestamp.png to clipboard"
+xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/"$timestamp".png
+notify-send -t 1000 "Copied file $timestamp.png to clipboard"
