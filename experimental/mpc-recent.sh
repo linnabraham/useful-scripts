3e11303ed8a8e774595762b3eadd2b8b5ad5cdac
linnabraham <linn.official@gmail.com>
2024-10-30 11:17:11 +0530

    Add ability to optionally give genre

--- experimental/mpc-recent.sh
+++ experimental/mpc-recent.sh
@@ -1,15 +1,20 @@
 #!/bin/env bash
 # Script for listing out some in my music database more recent than a given year
 from=$1
+genre=$2
 mpc list Date | grep -E '^[0-9]{4}' | while read date; do
 
   # year="${date:0:4}"
   # echo "Processing date: '$date' (Year: $year)"
   #if [[ "${date:0:4}" -gt $from ]]; then
   if (( "${date:0:4}" > $from )); then
-    mpc find "((Genre == \"Malayalam\") AND (Date == \"$date\"))"
+	  if [[ "$2" != "" ]]; then
+    		mpc find "((Genre == \"${2}\") AND (Date == \"$date\"))"
+	else
+    mpc find "(Date == \"$date\")"
 # else
 	# echo "Problematic" "$date"
+	  fi
   fi
 done
 # done | mpc add
