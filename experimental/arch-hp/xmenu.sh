cf4431669cd88cba5b13c68ed11a1648bb47dcf5
linnabraham <linn.official@gmail.com>
2025-04-17 14:45:35 +0530

    Update xmenu

--- experimental/arch-hp/xmenu.sh
+++ experimental/arch-hp/xmenu.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
-
 cat <<EOF | xmenu | sh &
 Apps
-	Terminal	xfce4-terminal
-	Music Player	mpdevil
-	Xournal	        xournalpp
+	PCManFM-Qt	pcmanfm-qt
 	Amplenote	gtk-launch brave-pfphgelppkenhllngngioolkaeelhlmi-Default
 	Brave	        brave
 	Zotero	        gtk-launch zotero
 	Popcorn-Time	popcorntime
-	iBroadcast	mediasynclite
+	Terminal	xfce4-terminal
+	Telegram	telegram-desktop
+Appmenu	jgmenu_run
 Music
-	Malayalam	mpc clear; mpc ls fixed/A-Z_fixed | mpc add; mpc shuffle; mpc play
-	Tamil	        mpc clear; mpc ls fixed/tamil_fixed | mpc add; mpc shuffle; mpc play
-	Hindi	        mpc clear; mpc ls fixed/hindi_fixed | mpc add; mpc shuffle; mpc play
-	English	        mpc clear; mpc ls fixed/eng_fixed | mpc add; mpc shuffle; mpc play
-	All		mpc clear; mpc ls fixed/ | mpc add; mpc shuffle; mpc play
-	Recent		cd ~/Music && find . -type f -mtime -30  | awk '{ sub(/^\.\//, ""); print }' | mpc add
+	Player		mpdevil
+	iBroadcast	mediasynclite
+	Picard		picard
+	Puddletag	puddletag
+	Playlist
+		Malayalam	mpc clear; mpc ls fixed/A-Z_fixed | mpc add; mpc shuffle; mpc play
+		Tamil	        mpc clear; mpc ls fixed/tamil_fixed | mpc add; mpc shuffle; mpc play
+		Hindi	        mpc clear; mpc ls fixed/hindi_fixed | mpc add; mpc shuffle; mpc play
+		English	        mpc clear; mpc ls fixed/eng_fixed | mpc add; mpc shuffle; mpc play
+		Recent		cd ~/Music && find . -type f -mtime -30  | awk '{ sub(/^\.\//, ""); print }' | mpc add; mpc play
+		All		mpc clear; mpc ls fixed/ | mpc add; mpc shuffle; mpc play
 Actions
-	New-workspace	i3-msg workspace $(($(i3-msg -t get_workspaces | jq 'max_by(.num) | .num')+1))
+	Display Off	xset dpms force off
+	New-workspace	switch-i3-workspace.sh
 	Screen Record	rofi-screenshot 
 	X-Terminate	xkill
 	Screenshot	xmenu-screenshot.sh
 	Polybar Toggle		xdo id -m -N Polybar && polybar-msg cmd toggle
 Sticky-Note	globonote
 
-PCManFM-Qt	pcmanfm-qt
 Refresh-Wallpaper	        	setbg -s "$HOME/Downloads/wallpapers"
+Lock	blurlock.sh
 Logout	lxsession-logout
 EOF
